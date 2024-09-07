d3549e965d5fea59c15334c4036ea33bbbf79edf
RIxi <shaiksuhaib360@gmail.com>
2024-09-07 11:50:37 +0530

    Add files via upload

---
+++ problem9.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    int year;
+    printf("Enter your year:");
+    scanf("%d",&year);
+    if(year %4==0 && year %100!=0 || year %400==0){
+        printf("This is a leap year!");
+    }
+    else {
+        printf("This is not a leap year");
+    }
+    return 0;
+}
